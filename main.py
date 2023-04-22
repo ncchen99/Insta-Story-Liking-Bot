590243e61238329406572373a7ec30c065602c7b
ncchen99 <ncchen99@gmail.com>
2023-04-22 21:44:15 +0800

    🥃 Finish basic feature

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from instagrapi import Client
 from dotenv import load_dotenv
 from time import sleep
 from rich.console import Console
+from rich.progress import track
+from pprint import pprint
 
 load_dotenv()
 
@@ -18,8 +20,8 @@ class Animator():
     def __init__(self):
         self.console = Console()
         self.contents = {"login": ":smirk_cat: [bold cyan]Initialized[/bold cyan] instagrapi",
-                         "get followers": ":smiling_imp: [bold #48C9B0 ]Login[/bold #48C9B0 ] successful",
-                         "done": ":alien: [bold #AF7AC5]Get followers[/bold #AF7AC5] complete"}
+                         "get following": ":smiling_imp: [bold #48C9B0 ]Login[/bold #48C9B0 ] successful",
+                         "done": ":alien: [bold #AF7AC5]Get following[/bold #AF7AC5] complete"}
         
     def createStatus(self):
         self.stat = self.console.status(
@@ -32,6 +34,8 @@ class Animator():
             status=f"[bold green]Working on {statusText}...")
 
 ani = Animator()
+cl = None
+console = Console()
 with ani.createStatus() as status:
     # initialize
     cl = Client()
@@ -39,8 +43,23 @@ with ani.createStatus() as status:
 
     # Login
     cl.login(username, password, verification_code=ERFA_code)
-    ani.update("get followers")
+    ani.update("get following")
 
-    # Get the list of followers
-    followers = cl.user_followers(cl.user_id)
+    # Get the list of following
+    followers = cl.user_following(cl.user_id)
     ani.update("done")
+
+
+storys = []
+
+console.print("💀 [bold #EB984E]Getting[bold #EB984E] all users story")
+for follower_id in track(followers, description=""):
+    storys.extend([i.dict()["id"] for i in cl.user_stories(follower_id)])
+
+console.print("💓 [bold #F458F6]Liking storys[bold #F458F6]")
+for id in track(storys, description=""):
+    cl.story_like(id)
+    
+
+
+    
