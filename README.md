d23a76ab628336004afb7ef4fbae2d2eb8396814
ncchen99 <ncchen99@gmail.com>
2023-05-18 01:13:54 +0800

    🚀 Refactoring and boost up efficiency using threading

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Insta-Story-Liking-Bot
-IG ㄉ限動按讚機器人 使用 instagrapi
+# 💓 Insta-Story-Liking-Bot
+
+> IG ㄉ限動按讚機器人
+> 
+> 使用 [instagrapi](https://adw0rd.github.io/instagrapi/)
+> 
+> ⚠ [注意] 使用這個可能會被 ban 帳號💀
+
+### 🍀 Step1. 在這個資料夾下創建 `.env` 檔案
+```
+ACCOUNT=帳號id
+PASSWORD=密碼
+ERFA=true # false 有沒有啟用 2fa
+```
+> ⚠  建議使用 2fa 登入 
+
+### ☠ Step2. 執行 `main.py`
+
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
+import queue
+import threading
 from instagrapi import Client
 from dotenv import load_dotenv
 from time import sleep
 from rich.console import Console
 from rich.progress import track
+from rich.progress import Progress
 from pprint import pprint
+from lib import get_storys_worker
+from lib import like_storys_worker
+from lib import animator
 
 load_dotenv()
 
@@ -16,26 +22,8 @@ ERFA_code = ""
 if ERFA:
     ERFA_code = input("🚀 Input verification code:")
 
-class Animator():
-    def __init__(self, console):
-        self.console = console
-        self.contents = {"login": ":smirk_cat: [bold cyan]Initialized[/bold cyan] instagrapi",
-                         "get following": ":smiling_imp: [bold #48C9B0 ]Login[/bold #48C9B0 ] successful",
-                         "done": ":alien: [bold #AF7AC5]Get following[/bold #AF7AC5] complete"}
-        
-    def createStatus(self):
-        self.stat = self.console.status(
-            status=f"[bold green]Working on initial...", spinner="moon")
-        return self.stat
-    
-    def update(self, statusText):
-        self.console.print(self.contents[statusText])
-        self.stat.update(
-            status=f"[bold green]Working on {statusText}...")
-
-
 console = Console()
-ani = Animator(console)
+ani = animator.Animator(console)
 cl = None
 
 with ani.createStatus() as status:
@@ -48,20 +36,48 @@ with ani.createStatus() as status:
     ani.update("get following")
 
     # Get the list of following
-    followers = cl.user_following(cl.user_id)
+    following = cl.user_following(cl.user_id)
     ani.update("done")
 
 
-storys = []
+work_following = queue.Queue()
+work_following.queue = queue.deque(following.keys())
+work_storys = queue.Queue()
 
-console.print("💀 [bold #EB984E]Getting[bold #EB984E] all users story")
-for follower_id in track(followers, description=""):
-    storys.extend([i.dict()["id"] for i in cl.user_stories(follower_id)])
+# 建立 lock
+lock = threading.Lock()
 
-console.print("💓 [bold #F458F6]Liking storys[bold #F458F6]")
-for id in track(storys, description=""):
-    cl.story_like(id)
+workers = []
+
+###############################################################
+
+with Progress() as progress:
+
+    task_following = progress.add_task("💀 [bold #EB984E]Getting[bold #EB984E] all users story", total=work_following.qsize())
     
+    # 產生 20 個 worker
+    for i in range(20):
+        workers.append(get_storys_worker.Worker(work_following, lock, progress, task_following, work_storys, cl))
+        workers[-1].start()
+
+    for worker in workers:
+        worker.join()
+
+    ################################################################
+
+    task_storys = progress.add_task("💓 [bold #F458F6]Liking storys[bold #F458F6]", total=work_storys.qsize())
+
+    workers.clear()
+
+    # 產生 15 個 worker
+    for i in range(15):
+        workers.append(like_storys_worker.Worker(work_storys, lock, progress, task_storys, cl))
+        workers[-1].start()
+
+    for worker in workers:
+        worker.join()
+
+        
 
 
-    
+        
